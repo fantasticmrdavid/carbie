839ca1eb2701d3cffc357dcd3662c1a161ad3d3d
David Li <fantasticmrdavid@gmail.com>
2023-10-04 08:25:55 +1100

    Fix carbCalculator responsive

--- app/components/CarbCalculator/CarbCalculator.tsx
+++ app/components/CarbCalculator/CarbCalculator.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Flex, Input } from '@chakra-ui/react'
 import { Ingredient } from '@prisma/client'
+import styles from './carbCalculator.module.scss'
 
 type Props = {
   ingredient: Ingredient
@@ -9,23 +10,27 @@ type Props = {
 export const CarbCalculator = ({ ingredient }: Props) => {
   const [carbCalcValue, setCarbCalcValue] = useState<number | undefined>()
   return (
-    <Flex whiteSpace={'nowrap'} alignItems={'center'} gap={'0.2em'}>
-      How much should I eat for{' '}
-      <Input
-        textAlign={'right'}
-        width={'70px'}
-        type={'number'}
-        onChange={(e) =>
-          setCarbCalcValue(
-            e.target.value ? parseFloat(e.target.value) : undefined,
-          )
-        }
-      />
-      g of carbs?
+    <Flex className={styles.carbCalculator}>
+      <Flex alignItems={'center'} gap={'0.2em'}>
+        How much should I eat for{' '}
+        <Input
+          textAlign={'right'}
+          width={'70px'}
+          type={'number'}
+          onChange={(e) =>
+            setCarbCalcValue(
+              e.target.value ? parseFloat(e.target.value) : undefined,
+            )
+          }
+        />
+        g of carbs?
+      </Flex>
       {carbCalcValue && carbCalcValue > 0 && (
-        <strong>
-          {((100 / ingredient.carbs_per_100g) * carbCalcValue).toFixed(2)}g
-        </strong>
+        <Flex alignItems={'center'}>
+          <strong>
+            {((100 / ingredient.carbs_per_100g) * carbCalcValue).toFixed(2)}g
+          </strong>
+        </Flex>
       )}
     </Flex>
   )
